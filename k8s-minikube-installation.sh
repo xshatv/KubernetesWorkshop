980e3165de62353ee010aa8fa473eacd9d85f8be
Akshat Vishwamitra <151455425+xshatv@users.noreply.github.com>
2024-08-10 12:04:22 +0530

    Update k8s-minikube-installation.sh

--- k8s-minikube-installation.sh
+++ k8s-minikube-installation.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 # Installation of DOCKER
 # Add the official Docker GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
 # Add the Docker repository:
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # Install the necessary dependencies:
